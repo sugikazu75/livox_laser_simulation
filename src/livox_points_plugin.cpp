fc0d7909fdf16bca17f01f7b746d8a1908fd36da
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2024-06-11 12:22:31 +0900

    note the bug of ray calculation

--- src/livox_points_plugin.cpp
+++ src/livox_points_plugin.cpp
@@ -98,7 +98,7 @@ void LivoxPointsPlugin::Load(gazebo::sensors::SensorPtr _parent, sdf::ElementPtr
         ignition::math::Quaterniond ray;
         ray.Euler(ignition::math::Vector3d(0.0, rotate_info.zenith, rotate_info.azimuth));
         auto axis = offset.Rot() * ray * ignition::math::Vector3d(1.0, 0.0, 0.0);
-        start_point = minDist * axis + offset.Pos();
+        start_point = minDist * axis + offset.Pos(); // this would be bug
         end_point = maxDist * axis + offset.Pos();
         rayShape->AddRay(start_point, end_point);
     }
